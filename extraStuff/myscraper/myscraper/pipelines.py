e81669c00c740ed824c074956e3d877d2a33509c
GoSSIP <gossip@GoSSIPdeMacBook-Air.local>
2015-04-12 19:16:16 +0800

    add scraper and sslscan scripts

---
+++ extraStuff/myscraper/myscraper/pipelines.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import json
+class MyscraperPipeline(object):
+	def __init__(self):
+		self.file = open('domain', 'a')
+	def process_item(self, item):
+		r = []
+		#line = json.dumps(dict(item)) + "\n"
+		l = {}.fromkeys(item['link']).keys()
+		for i in l:
+			r.append(i.split('=')[1])
+		self.file.write('\n'.join(r))
+		return item
---
+++ extraStuff/myscraper/myscraper/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for myscraper project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'myscraper'
+
+SPIDER_MODULES = ['myscraper.spiders']
+NEWSPIDER_MODULE = 'myscraper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'myscraper (+http://www.yourdomain.com)'
